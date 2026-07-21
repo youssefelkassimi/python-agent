a8644b5566e7a45e7b9e51ed904c3b1abd46b023
EL KASSIMI YOUSSEF <elkassimi.you.fst@uhp.ac.ma>
2026-07-21 18:24:23 +0100

    Change disk usage metric to use root directory

--- collectors/system_metrics.py
+++ collectors/system_metrics.py
@@ -4,7 +4,7 @@ def get_system_metrics():
     return{
         "cpu_percent": psutil.cpu_percent(interval=1),
         "memory_percent": psutil.virtual_memory().percent,
-        "disk_usage_percent": psutil.disk_usage('c').percent,
+        "disk_usage_percent": psutil.disk_usage('/').percent,
         "load_avg": psutil.getloadavg() if hasattr(psutil, "getloadavg") else None,
 
     }
